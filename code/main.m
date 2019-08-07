fc6e4fcfe3e6996c534c8f31b8f0c96b8d13be1b
YunjeongLee <yunjung0104@gmail.com>
2019-08-07 13:38:08 +0900

    change values from yearly to daily

--- code/main.m
+++ code/main.m
@@ -1,10 +1,10 @@
 clear; close all; clc;
 
 %% Parameter values
-age_grps = [0, 2/12, 4/12, 6/12, 1, 18/12, 2:14, 15:5:50, 55, 65];
-vaccine_age = [2/12, 4/12, 6/12, 18/12, 6];
+age_grps = [0, 2/12, 4/12, 6/12, 1, 18/12, 2:14, 15:5:50, 55, 65]*365;
+vaccine_age = [2/12, 4/12, 6/12, 18/12, 6]*365;
 num_grps_val = length(age_grps);
-pop_age = diff([age_grps, 75]')/75;
+pop_age = diff([age_grps, 75*365]')/75/365*N;
 
 % Initial condition
 S0_val = 0.99 * pop_age;
@@ -18,18 +18,18 @@ I30_val = zeros(num_grps_val, 1);
 R0_val = zeros(num_grps_val, 1);
 
 % Parameters
-sigma_val = 1/11;
-tau_val = 1/2;
-tauP_val = 1/2;
-sigma0_val = 1/100;
+sigma_val = 1/11/365;
+tau_val = 1/2/365;
+tauP_val = 1/2/365;
+sigma0_val = 1/100/365;
 VE = 0.9;
 p_val = [0.95 * ones(3, 1); 0.85; 0.95];
 mu_val = [zeros(num_grps_val-1, 1); 1];
 rho1_val = 0.5;
 rho2_val = 0.25;
-gamma_val = 365/21;
+gamma_val = 1/21;
 c_val = 1./(diff(age_grps))';
-B_val = 1/75;
+B_val = 1/75/365;
 
 %% Generate vaccine vector
 vacc_val = zeros(num_grps_val, 1);
