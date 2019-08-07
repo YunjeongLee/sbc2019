069bed6bb4acadedc9a7f99b3545102905742fb6
YunjeongLee <yunjung0104@gmail.com>
2019-08-07 23:18:54 +0900

    add `xlims` as input

--- code/visualize.m
+++ code/visualize.m
@@ -1,4 +1,4 @@
-function [] = visualize(incd, time_stamp, group_for_label, text_for_legend, ylims)
+function [] = visualize(incd, time_stamp, group_for_ttl, text_for_legend, xlims, ylims)
 %% Plot the graphs
 figure('pos', [10 10 1600 900]);
 plot(time_stamp(2:end)/365, incd * 1e6, 'linewidth', 2)
@@ -8,5 +8,6 @@ ylabel(sprintf( ...
     'Incidence of %s age group (cases/year per 1,000,000 population)', ...
     group_for_label))
 legend(text_for_legend)
+xlim(xlims)
 ylim(ylims)
 set(gca, 'fontsize', 20);
