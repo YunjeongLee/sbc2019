04863dda46bcc6eee3494b5ce116017aa5a774d0
YunjeongLee <yunjung0104@gmail.com>
2019-08-07 21:24:50 +0900

    grid minor

--- code/visualize.m
+++ code/visualize.m
@@ -2,6 +2,7 @@ function [] = visualize(incd, time_stamp, group_for_label, text_for_legend, ylim
 %% Plot the graphs
 figure('pos', [10 10 1600 900]);
 plot(time_stamp(2:end)/365, incd * 1e6, 'linewidth', 2)
+grid on; grid minor;
 xlabel('time (years)')
 ylabel(sprintf( ...
     'Incidence of %s month age group (cases/year per 1,000,000 population)', ...
