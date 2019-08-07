c6329e19502fee1f703283e54c28cfa7ae47dbde
YunjeongLee <yunjung0104@gmail.com>
2019-08-07 23:19:03 +0900

    fix typo

--- code/visualize.m
+++ code/visualize.m
@@ -1,7 +1,7 @@
 function [] = visualize(incd, time_stamp, group_for_ttl, text_for_legend, xlims, ylims)
 %% Plot the graphs
 figure('pos', [10 10 1600 900]);
-plot(time_stamp(2:end)/365, incd * 1e6, 'linewidth', 2)
+plot(time_stamp(1:end)/365, incd * 1e6, 'linewidth', 2)
 grid on; grid minor;
 xlabel('time (years)')
 ylabel(sprintf( ...
