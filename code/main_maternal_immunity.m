0e1f8bacaff8b85898e67b8a3c364c201ec8277c
YunjeongLee <yunjung0104@gmail.com>
2019-08-08 23:25:09 +0900

    use only incidence from I1

--- code/main_maternal_immunity.m
+++ code/main_maternal_immunity.m
@@ -92,7 +92,7 @@ for i = 1:length(frac_immune_mother_range)
     [~, sol] = ode45(fode, time_stamp, y0, options);
 
     % Get incidence
-    incd = get_incidence(sol, params_temp);
+    [incd, ~] = get_incidence(sol, params_temp);
     incd_aggregate_baby(:,i) = sum(incd(:, 1), 2);
     incd_aggregate_all(:,i) = sum(incd, 2);
 end
