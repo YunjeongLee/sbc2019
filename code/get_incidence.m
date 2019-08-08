ce193a4d82ca21e0da7461a183ab8b5a2826d97b
YunjeongLee <yunjung0104@gmail.com>
2019-08-08 15:53:42 +0900

    delete useless lines

--- code/get_incidence.m
+++ code/get_incidence.m
@@ -13,26 +13,10 @@ I1 = sol(:, 5*num_grps_+1:6*num_grps_);
 I2 = sol(:, 6*num_grps_+1:7*num_grps_);
 I3 = sol(:, 7*num_grps_+1:8*num_grps_);
 
-dt = time_stamp(2) - time_stamp(1);
-
 %% Incidence for I1
-% previous_lambda = contact_matrix_ ...
-%     * (I1(1:end-1, :)' + rho1_ * I2(1:end-1, :)' + rho2_ * I3(1:end-1, :)');
-% previous = previous_lambda .* S(1:end-1, :)';
-% next_lambda = contact_matrix_ ...
-%     * (I1(2:end, :)' + rho1_ * I2(2:end, :)' + rho2_ * I3(2:end, :)');
-% next = next_lambda .* S(2:end, :)';
-% incdI1 = ((previous + next)/2 * dt)';
 incdI1 = contact_matrix_ * (I1' + rho1_ * I2' + rho2_ * I3') .* S';
 
 %% Incidence for I2
-% previous_lambda = contact_matrix_ ...
-%     * (I1(1:end-1, :)' + rho1_ * I2(1:end-1, :)' + rho2_ * I3(1:end-1, :)');
-% previous = previous_lambda .* P1(1:end-1, :)';
-% next_lambda = contact_matrix_ ...
-%     * (I1(2:end, :)' + rho1_ * I2(2:end, :)' + rho2_ * I3(2:end, :)');
-% next = next_lambda .* P1(2:end, :)';
-% incdI2 = ((previous + next)/2 * dt)';
 incdI2 = contact_matrix_ * (I1' + rho1_ * I2' + rho2_ * I3') .* P1';
 
 %% Merge two incidences
